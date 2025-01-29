e5a550fdbfe814fab9aa8668fd0407ce8f46ee5c
Speedy <125962994+mordehai10@users.noreply.github.com>
2025-01-29 11:45:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
-const form = document.getElementById('meetup-form');
-
-form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-
-    const name = document.getElementById('name').value;
-    const dates = document.getElementById('dates').value;
-
-    // Send data to Google Sheet using Google Apps Script
-    const response = await fetch('https://script.google.com/macros/s/AKfycbyHivdCVJGe5_ABuW5WCfh710z2eRnjTGg6srMfSXSYztBueBD-l76bWmx4aEMEwGNH/exec', {
-        method: 'POST',
-        body: JSON.stringify({ name, dates }),
-        headers: { 'Content-Type': 'application/json' }
-    });
-
-    const data = await response.json();
-
-    if (data.success) {
-        alert('Your information has been submitted successfully!');
-        // Clear the form after successful submission
-        form.reset();
-    } else {
-        alert('There was an error submitting your information. Please try again.');
-    }
-});
-
 $(document).ready(function() {
     $("#dates").datepicker({
         multiDate: true,
-        dateFormat: "yy-mm-dd" // Format the date as YYYY-MM-DD for Google Sheets integration
+        dateFormat: "yy-mm-dd" 
+    });
+
+    const form = document.getElementById('meetup-form');
+
+    form.addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        const name = document.getElementById('name').value;
+        const dates = document.getElementById('dates').value;
+
+        try {
+            const response = await fetch('https://script.google.com/macros/s/AKfycbyHivdCVJGe5_ABuW5WCfh710z2eRnjTGg6srMfSXSYztBueBD-l76bWmx4aEMEwGNH/exec', {
+                method: 'POST',
+                body: JSON.stringify({ name, dates }),
+                headers: { 'Content-Type': 'application/json' }
+            });
+
+            const data = await response.json();
+
+            if (data.success) {
+                alert('Your information has been submitted successfully!');
+                form.reset();
+            } else {
+                alert('There was an error submitting your information. Please try again.');
+            }
+
+        } catch (error) {
+            console.error('Error:', error);
+            alert('An error occurred. Please try again later.');
+        }
     });
 });
